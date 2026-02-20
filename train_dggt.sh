33601d4e23c73e004aaca52612d33d46c2aa7691
Lulin Liu <93459201+lulinliu@users.noreply.github.com>
2026-02-19 23:51:58 -0500

    Change default NNODES value from 8 to 16

--- train_dggt.sh
+++ train_dggt.sh
@@ -6,17 +6,16 @@ cd /scratch/10102/hh29499/longtail_train/dggt_tacc
 
 MASTER_ADDR="${MASTER_ADDR:-$(scontrol show hostnames "${SLURM_JOB_NODELIST}" | head -n1)}"
 MASTER_PORT="${MASTER_PORT:-29501}"
-NNODES="${SLURM_NNODES:-${SLURM_JOB_NUM_NODES:-8}}"
+NNODES="${SLURM_NNODES:-${SLURM_JOB_NUM_NODES:-16}}"
 NODE_RANK="${SLURM_NODEID:?SLURM_NODEID is not set}"
 
-export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0}"
 export PYTORCH_CUDA_ALLOC_CONF="${PYTORCH_CUDA_ALLOC_CONF:-expandable_segments:True,max_split_size_mb:256,garbage_collection_threshold:0.8}"
 
 LOG_DIR="/scratch/10102/hh29499/longtail_train/dggt_tacc/logs"
 mkdir -p "${LOG_DIR}"
 LOG="${LOG_DIR}/train_${SLURM_JOB_ID:-manual}_node${NODE_RANK}_$(date +%Y%m%d_%H%M%S).log"
 
-echo "[torchrun] MASTER_ADDR=${MASTER_ADDR} MASTER_PORT=${MASTER_PORT} NNODES=${NNODES} NODE_RANK=${NODE_RANK} CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}"
+echo "[torchrun] MASTER_ADDR=${MASTER_ADDR} MASTER_PORT=${MASTER_PORT} NNODES=${NNODES} NODE_RANK=${NODE_RANK}"
 
 TORCHRUN_CMD=(
   torchrun
